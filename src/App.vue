70251dadeeb0e6644932a0c2f46ad74c80efdbe4
Zach Earl <zachearl@GOV-ZE-MACBOOKPRO13.local>
2021-03-19 11:34:20 -0400

    test app for hosting

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,12 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <h2><router-link to="/index">Subscriptions</router-link></h2>
+        <router-link to="/create" class="btn btn-sm btn-primary">Add subscription</router-link>
+
+        <router-view />
+      </div>
     </div>
-    <router-view/>
   </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    redirect: '/index'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: "/create",
+    name: "create",
+    component: () => import("../components/Create.vue")
+  },
+  {
+    path: "/edit/:id",
+    name: "edit",
+    component: () => import("../components/Edit.vue")
+  },
+  {
+    path: "/index",
+    name: "index",
+    component: () => import("../components/Index.vue")
+  },
 ]
 
 const router = new VueRouter({
